5db7069a80b2ac3456d1569c8914a7cc83a280f1
Mateusz Antkiewicz <mateusz.gamewin1@gmail.com>
2024-12-08 00:47:35 +0100

    Optimize and parallel test

--- day7/part2par.py
+++ day7/part2par.py
@@ -1,5 +1,6 @@
 
 import time
+import multiprocessing as mp
 
 def solve(state: int, nums: list, op_count: int, i: int, type: int, target: int):
     if type == 0:
@@ -13,7 +14,7 @@ def solve(state: int, nums: list, op_count: int, i: int, type: int, target: int)
             decimal_shift = 3
         elif nums[i] >= 10:
             decimal_shift = 2
-        
+            
         state = state * (10**decimal_shift) + nums[i]
         
     if i == op_count:
@@ -27,14 +28,18 @@ def solve(state: int, nums: list, op_count: int, i: int, type: int, target: int)
         else:
             return max(solve(state, nums, op_count, i+1, 0, target), solve(state, nums, op_count, i+1, 1, target), solve(state, nums, op_count, i+1, 2, target))
 
-start = time.time()
-lines = list(map(lambda l: l.strip(), open('input.txt').readlines()))
-equations = list(map(lambda eq: (int(eq[0]), list(map(int, eq[1].strip().split(' ')))), list(map(lambda l: l.split(':'), lines))))
+def process(pair: tuple[int, list]):
+    return solve(0, pair[1], len(pair[1]) - 1, 0, 0, pair[0])
 
-result = 0
-for (target, numbers) in equations:
-    result += solve(0, numbers, len(numbers) - 1, 0, 0, target)
-        
-end = time.time()
-        
-print(f"{result}, took: {(end - start) * 1000.0}ms")
+if __name__ == '__main__':
+    start = time.time()
+    lines = list(map(lambda l: l.strip(), open('input.txt').readlines()))
+    equations = list(map(lambda eq: (int(eq[0]), list(map(int, eq[1].strip().split(' ')))), list(map(lambda l: l.split(':'), lines))))
+    
+    result = 0
+    with mp.Pool(mp.cpu_count()-4) as p:
+        result = sum(p.map(process, equations))
+
+    end = time.time()
+
+    print(f"{result}, took: {(end - start) * 1000.0}ms")
